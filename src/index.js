ca55f23423c6bd76238d4693a38343e50b5cc0b9
Stacy Dillahey <stacy.dillahey@gmail.com>
2017-06-04 17:32:34 -0700

    add express and other npm modules

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+    BrowserRouter,
+    Link,
+    Switch,
+    Route
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import { createStore } from 'redux';
+import reducer from './reducers/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducer);
+
+ReactDOM.render((
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+    ), document.getElementById('root'));
