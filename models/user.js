e902b6bea8aa6976cfdc1e450bc49d860d0e33c2
Stacy Dillahey <stacy.dillahey@gmail.com>
2017-06-05 10:50:47 -0700

    edit user model to include boards

--- models/user.js
+++ models/user.js
@@ -1,13 +1,31 @@
- var mongoose = require('mongoose');
- const Schema = mongoose.Schema;
- 
- var userSchema = new Schema({
-   name: String,
-   email: String,
-   avatar: String,
-   googleId: String,
-   boards: [],
-   created: { type: Date, default: Date.now }
- });
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
- module.exports = mongoose.model('User', userSchema);
+var boardSchema = new Schema({
+  // add multiple users to share with friends?
+  users: [String],
+  city: String,
+  image: String,
+  recs: {
+        activities: [Object],
+        restaurants: [Object],
+        hotels: [Object],
+        events: [Object],
+        nextstops: [String],
+        reqreading: [String],
+        transportation: [String],
+        needtoknow: [String]
+  },
+  notes: [String]
+});
+ 
+var userSchema = new Schema({
+  name: String,
+  email: String,
+  avatar: String,
+  googleId: String,
+  boards: [boardSchema],
+  created: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', userSchema);
