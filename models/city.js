d07d9e0dfb158db2456ca834d565d1933963e2bd
Stacy Dillahey <stacy.dillahey@gmail.com>
2017-06-05 10:42:04 -0700

    create city model

---
+++ models/city.js
@@ -0,0 +1,40 @@
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+ 
+var reckSchema = new Schema ({
+  name: String,
+  image: String,
+  address: String,
+  description: String,
+  votecount: Number,
+  comments: [String]
+})
+
+var eventSchema = new Schema ({
+  name: String,
+  month: { type: Number, enum: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] },
+  blurb: String
+})
+
+var citySchema = new Schema({
+    city: String,
+    country: String,
+    continent: String,
+    region: [String],
+    tags: [String],
+    heximg: String,
+    img: String,
+    recs: {
+        activities: [reckSchema],
+        restaurants: [reckSchema],
+        hotels: [reckSchema],
+        instagram: [String],
+        events: [eventSchema],
+        nextstops: [String],
+        reqreading: [String],
+        transportation: [String],
+        needtoknow: [String]
+    }
+ });
+
+module.exports = mongoose.model('City', citySchema);
